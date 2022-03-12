a8a297ff4a3b266cca3328fa0eb4c237b666d8ae
Calvin Jee <calvin.jee@tevosol.com>
2022-03-12 12:23:39 -0700

    aoc_2021_03a - loop counter, vector processing, output - confirmed

--- AdventOfCode/2021_day3/aoc_2021_03a.cpp
+++ AdventOfCode/2021_day3/aoc_2021_03a.cpp
@@ -11,11 +11,62 @@
 #include <iostream>
 #include <fstream>
 #include "../Libraries/aocFile.h"
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    exampleFunc(0);
+    // read file
+    std::ios::sync_with_stdio(false);
+    ifstream fileInput;
+    if (loadFile(fileInput))
+    {
+        fileInput.close();
+        return 0;
+    }
+
+    // declare vars
+    string binStr;
+    uint16_t binVal = 0, count = 0, oneBit = 1, binStrLen = 0;
+    vector<uint16_t> vec(sizeof(uint16_t) * 8, 0);
+
+    // read loop
+    while (fileInput >> binStr)
+    {
+        if (count == 0)
+        {
+            binStrLen = binStr.length();
+        }
+        
+        binVal = stoi(binStr, 0, 2);
+        for (int i = 0; i < binStrLen; i++)
+        {
+            if (binVal & oneBit << i)
+            {
+                vec[i]++;
+            }
+        }
+        count++;
+    }
+
+    // process vector data
+    uint16_t gamma = 0, epsilon = 0;
+    for (int i = 0; i < binStrLen; i++)
+    {
+        if (vec[i] > count / 2)
+        {
+            gamma = gamma | oneBit << i;
+        }
+        else
+        {
+            epsilon = epsilon | oneBit << i;
+        }
+    }
+
+    // final output
+    cout << epsilon * gamma << '\n';
+    fileInput.close();
     return 0;
 }
