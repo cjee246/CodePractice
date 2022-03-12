a4f7d8429609ea2974a9a3075cb71f05f15d9176
Calvin Jee <calvin.jee@tevosol.com>
2022-03-12 11:18:54 -0700

    aoc_2021_03a - setup json and include

---
+++ AdventOfCode/2021_day3/aoc_2021_03a.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author: Calvin Jee
+ * Email: cjee246@gmail.com
+ *
+ * Date: 2022-03-12
+ */
+
+/******************************************************************************/
+/* HEADER FILES */
+/******************************************************************************/
+#include <iostream>
+#include <fstream>
+#include "../Libraries/aocFile.h"
+
+using namespace std;
+
+int main()
+{
+    exampleFunc(0);
+    return 0;
+}
