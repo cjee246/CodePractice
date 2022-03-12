d7df2d79e13eb3fb31725bd225c34c76ca2e4eaa
Calvin Jee <calvin.jee@tevosol.com>
2022-03-11 21:42:27 -0700

    aoc_2021_02a env setup

---
+++ AdventOfCode/2021_day2/aoc_2021_02a.cpp
@@ -0,0 +1,22 @@
+/*
+ * Author: Calvin Jee
+ * Email: cjee246@gmail.com
+ *
+ * Date: 2022-03-11
+ */
+
+/******************************************************************************/
+/* HEADER FILES */
+/******************************************************************************/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
