7c8b7eb7fe8158acdb8b5fcde732bbede966f97f
Calvin Jee <calvin.jee@tevosol.com>
2022-03-11 23:20:14 -0700

    aoc_2021_02a position loop and output - confirmed'

--- AdventOfCode/2021_day2/aoc_2021_02a.cpp
+++ AdventOfCode/2021_day2/aoc_2021_02a.cpp
@@ -19,12 +19,37 @@ using namespace std;
 
 int main()
 {
-    //apparently this makes iostream faster
-    //std::ios::sync_with_stdio(false);
-
+    // apparently this makes iostream faster
+    std::ios::sync_with_stdio(false);
     ifstream inputFile;
-    loadFile(inputFile);
+    if (loadFile(inputFile))
+    {
+        inputFile.close();
+        return 0;
+    }
 
+    // get position
+    uint32_t xPos = 0, zPos = 0, mult = 0;
+    string cmd, val;
+    while (inputFile >> cmd >> val)
+    {
+        if (cmd == "forward")
+        {
+            xPos += stoi(val);
+        }
+        else if (cmd == "down")
+        {
+            zPos += stoi(val);
+        }
+        else if (cmd == "up")
+        {
+            zPos -= stoi(val);
+        }
+    }
+    mult = xPos * zPos;
 
+    // finish
+    cout << mult << '\n';
+    inputFile.close();
     return 0;
 }
