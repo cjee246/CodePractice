9da5b8c93c8c8a0e6b0a260eab5f4b1b5fd48a6a
Calvin Jee <calvin.jee@tevosol.com>
2022-03-13 00:12:42 -0700

    aoc_2021_05a - long checking operation - confirmed'

--- AdventOfCode/2021_day5/aoc_2021_05a.cpp
+++ AdventOfCode/2021_day5/aoc_2021_05a.cpp
@@ -12,59 +12,50 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <array>
 #include <algorithm>
 #include <numeric>
 #include "../Libraries/aocFile.h"
 
 using namespace std;
 
-
-
-/******************************************************************************/
-/* GLOBAL VARS */
-/******************************************************************************/
-static ifstream fileInput;
-static vector<vector<vector<uint16_t>>> lines;
-
-
-
 /******************************************************************************/
 /* ADDITIONAL FUNCTIONS */
 /******************************************************************************/
-static void getLines()
+static void getLines(ifstream &filestream, vector<vector<vector<uint16_t>>> *lines)
 {
     string str, substr;
-    uint16_t x1 = 0, x2 = 0, y1 = 0, y2 = 0;
-    while (fileInput >> str)
+    uint16_t x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+    stringstream strStream;
+    while (filestream >> str)
     {
-        // start string stream
-        stringstream strStream(str);
-
         // get first pair from string
+        strStream.clear();
+        strStream << str;
         getline(strStream, substr, ',');
         x1 = stoi(substr);
         getline(strStream, substr, '\n');
         y1 = stoi(substr);
 
         // get second pair from file
-        fileInput >> str;
-        fileInput >> str;
+        filestream >> str;
+        filestream >> str;
 
         // get second pair from string
+        strStream.clear();
+        strStream << str;
         getline(strStream, substr, ',');
         x2 = stoi(substr);
-        getline(strStream, substr, '\n');
+        getline(strStream, substr);
         y2 = stoi(substr);
 
         // if vertical/horizontal, add to vector
         if (x1 == x2 || y1 == y2)
         {
-            lines.push_back( { {x1, y1}, {x2, y2} } );
+            (*lines).push_back({{x1, y1}, {x2, y2}});
         }
     }
 }
-
-
 
 /******************************************************************************/
 /* MAIN */
@@ -73,7 +64,7 @@ int main()
 {
     // read file
     std::ios::sync_with_stdio(false);
-
+    ifstream fileInput;
     if (loadFile(fileInput))
     {
         fileInput.close();
@@ -81,11 +72,54 @@ int main()
     }
 
     // read in vector pairs
-    getLines();
-    uint16_t count = 0;
+    vector<vector<vector<uint16_t>>> lines;
+    uint16_t grid[1000][1000] = { 0 };
+    uint16_t start = 0, end = 0, x = 0, y = 0;
+    getLines(fileInput, &lines);
+    for (int16_t i = 0; i < lines.size(); i++)
+    {
+        x = lines[i][0][0];
+        y = lines[i][0][1];
+        bool iseq = false;
+        while (x != lines[i][1][0] || y != lines[i][1][1])
+        {
+            grid[x][y]++;
+            if (x < lines[i][1][0])
+            {
+                x++;
+            }
+            if (x > lines[i][1][0])
+            {
+                x--;
+            }
+            if (y < lines[i][1][1])
+            {
+                y++;
+            }
+            if (y > lines[i][1][1])
+            {
+                y--;
+            }
+        }
+        grid[x][y]++;
+    }
 
-    //
+    //count numbers greater than or equal to x
+    uint16_t thresh = 2;
+    uint16_t count = 0;
+    for (uint16_t i = 0; i < sizeof(grid)/sizeof(grid[1]); i++)
+    {
+        for (uint16_t j = 0; j < sizeof(grid)/sizeof(grid[0]); j++)
+        {
+            if (grid[i][j] >= thresh)
+            {
+                count++;
+            }
+        }
+    }
+
+    // print and exit
+    cout << count << '\n';
     fileInput.close();
     return 0;
 }
-
