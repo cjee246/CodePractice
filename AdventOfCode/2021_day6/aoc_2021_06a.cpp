35b46cfb706abd728958dbce9c79c2f89cd1883e
Calvin Jee <calvin.jee@tevosol.com>
2022-03-13 10:43:20 -0600

    aoc_2021_06a - setup and first pass

---
+++ AdventOfCode/2021_day6/aoc_2021_06a.cpp
@@ -0,0 +1,82 @@
+/*
+ * Author: Calvin Jee
+ * Email: cjee246@gmail.com
+ *
+ * Date: 2022-03-13
+ */
+
+/******************************************************************************/
+/* HEADER FILES */
+/******************************************************************************/
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include "../Libraries/aocFile.h"
+
+using namespace std;
+
+/******************************************************************************/
+/* GLOBAL VARS */
+/******************************************************************************/
+#define DAYS 18u
+
+/******************************************************************************/
+/* ADDITIONAL FUNCTIONS */
+/******************************************************************************/
+static void func()
+{
+}
+
+/******************************************************************************/
+/* MAIN */
+/******************************************************************************/
+int main()
+{
+    // read file
+    std::ios::sync_with_stdio(false);
+    ifstream fileInput;
+    if (loadFile(fileInput))
+    {
+        fileInput.close();
+        return 0;
+    }
+
+    // init vector from string stream
+    vector<uint8_t> fish;
+    string str, substr;
+    fileInput >> str;
+    stringstream strStream(str);
+    while (strStream.good())
+    {
+        getline(strStream, substr, ',');
+        fish.push_back(stoi(substr));
+    }
+
+    // process fishies
+    for (uint8_t i = 0; i < DAYS; i++)
+    {
+        uint16_t fishCount = fish.size();
+        for (uint8_t j = 0; j < fishCount; j++)
+        {
+            if (fish[j] == 0)
+            {
+                fish.push_back(8);
+                fish[j] = 6;
+            }
+            else
+            {
+                fish[j]--;
+            }
+        }
+    }
+
+    // print and exit
+    cout << fish.size() << '\n';
+    fileInput.close();
+    return 0;
+}
