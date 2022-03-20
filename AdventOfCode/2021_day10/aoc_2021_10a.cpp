507b95d6f5079f8a58f1b91e622d7b72fe4e2dbd
Calvin Jee <calvin.jee@tevosol.com>
2022-03-19 22:54:30 -0600

    aoc_2021_10a - first pass at returning syntax error

--- AdventOfCode/2021_day10/aoc_2021_10a.cpp
+++ AdventOfCode/2021_day10/aoc_2021_10a.cpp
@@ -2,7 +2,7 @@
  * Author: Calvin Jee
  * Email: cjee246@gmail.com
  *
- * Date: YYYY-MM-DD
+ * Date: 2022-03-19
  */
 
 /******************************************************************************/
@@ -28,7 +28,7 @@ using namespace std;
 /******************************************************************************/
 /* FUNCTION DECLARATIONS */
 /******************************************************************************/
-static void func();
+static char checkSyntax(string line);
 
 /******************************************************************************/
 /* MAIN */
@@ -44,6 +44,17 @@ int main()
         return 0;
     }
 
+    // get vector of strings;
+    vector<string> vecStr;
+    vector<char> vecErr;
+    getStrVec(fileInput, &vecStr);
+    for (auto &str : vecStr)
+    {
+        vecErr.push_back(checkSyntax(str));
+    }
+
+    // parse brackets
+
     // print and exit
     cout << '\n';
     fileInput.close();
@@ -53,7 +64,59 @@ int main()
 /******************************************************************************/
 /* FUNCTION DEFINITIONS */
 /******************************************************************************/
-static void func()
+static char checkSyntax(string line)
 {
+    stringstream strStream;
+    vector<vector<char>> vecChar = {{'(', ')'},
+                                    {'[', ']'},
+                                    {'{', '}'},
+                                    {'<', '>'}};
+    vector<int16_t> count;
+    vector<vector<char>> order;
+    vector<vector<char>>::iterator it;
+    uint16_t idx;
+    char c_return;
+    for (uint16_t i = 0; i < line.size(); i++)
+    {
+        c_return = line.at(i);
+        for (uint8_t i = 0; i < vecChar.size(); i++)
+        {
+            if (c_return == vecChar[i][0] || c_return == vecChar[i][1])
+            {
+                // find index of char set
+                it = find(order.begin(), order.end(), vecChar[i]);
+                if (it != order.end())
+                {
+                    idx = distance(order.begin(), it);
+                }
+                else
+                {
+                    order.push_back(vecChar[i]);
+                    count.push_back(0);
+                    idx = order.size() - 1;
+                }
 
+                // acc count of char set
+                if (c_return == vecChar[i][0])
+                {
+                    count[idx]++;
+                }
+                else
+                {
+                    count[idx]--;
+                }
+
+                // check bracket closure
+                for (uint8_t j = 1; j < count.size(); j++)
+                {
+                    if (count[j] > count[j - 1])
+                    {
+                        return c_return;
+                    }
+                }
+                break;
+            }
+        }
+    }
+    return 'x';
 }
