aab9c88c5381ec686e0a747d619e531db65ab72e
Calvin Jee <calvin.jee@tevosol.com>
2022-03-20 11:31:28 -0600

    aoc_2021_11a - 2D fixes for fill and all_of

--- AdventOfCode/2021_day11/aoc_2021_11a.cpp
+++ AdventOfCode/2021_day11/aoc_2021_11a.cpp
@@ -30,7 +30,9 @@ using namespace std;
 /******************************************************************************/
 static void doStep(vector<vector<uint8_t>> *pGrid,
                    vector<vector<uint8_t>> *pExcite,
-                   vector<vector<uint8_t>> *pFlash);
+                   vector<vector<uint8_t>> *pFlash,
+                   uint64_t &flashCount);
+static void incAdj(vector<vector<uint8_t>> *pGrid, uint8_t x, uint8_t y);
 
 /******************************************************************************/
 /* MAIN */
@@ -71,14 +73,20 @@ int main()
 
     // do stuff
     bool update = true;
-    uint64_t count = 0;
+    uint64_t totalCount = 0;
     for (uint16_t i = 0; i < STEPS; i++)
     {
-        doStep(&vecGrid, &excGrid, &flashGrid);
+        uint64_t stepCount = 0;
+        for (auto &vec : excGrid)
+        {
+            fill(vec.begin(), vec.end(), 1);
+        }
+        doStep(&vecGrid, &excGrid, &flashGrid, stepCount);
+        totalCount += stepCount;
     }
 
     // print and exit
-    cout << '\n';
+    cout << totalCount << '\n';
     fileInput.close();
     return 0;
 }
@@ -88,16 +96,52 @@ int main()
 /******************************************************************************/
 static void doStep(vector<vector<uint8_t>> *pGrid,
                    vector<vector<uint8_t>> *pExcite,
-                   vector<vector<uint8_t>> *pFlash)
+                   vector<vector<uint8_t>> *pFlash,
+                   uint64_t &flashCount)
 {
-    
+    // excite grid
     for (uint16_t i = 0; i < (*pGrid).size(); i++)
     {
         for (uint16_t j = 0; j < (*pGrid)[i].size(); j++)
         {
-            (*pGrid)[i][j]++;
+            (*pGrid)[i][j] += (*pExcite)[i][j];
+            (*pExcite)[i][j] = 0;
             if ((*pGrid)[i][j] >= 9)
             {
+                if ((*pFlash)[i][j] == 0)
+                {
+                    (*pFlash)[i][j]++;
+                    flashCount++;
+                    incAdj(pExcite, i, j);
+                }
+            }
+        }
+    }
+
+    // do excitations
+    for (auto &vec : (*pExcite))
+    {
+        if (!all_of(vec.begin(), vec.end(),
+                    [](uint64_t n)
+                    { return n == 0; }))
+        {
+            doStep(pGrid, pExcite, pFlash, flashCount);
+            break;
+        }
+    }
+
+    // reset flash and grid
+    for (auto &vec : (*pFlash))
+    {
+        fill(vec.begin(), vec.end(), 0);
+    }
+    for (auto &vec : (*pGrid))
+    {
+        for (auto &elem : vec)
+        {
+            if (elem >= 9)
+            {
+                elem = 0;
             }
         }
     }
@@ -106,14 +150,18 @@ static void doStep(vector<vector<uint8_t>> *pGrid,
 static void incAdj(vector<vector<uint8_t>> *pGrid, uint8_t x, uint8_t y)
 {
     uint16_t xMax = (*pGrid).size();
+    uint16_t yMax = (*pGrid)[x].size();
     for (int8_t i = -1; i <= 1; i++)
     {
-        if ((x + i >= 0) && (x + i < (*pGrid).size()))
+        if (x + i >= 0 && (int16_t)x + i < xMax)
+        {
+            for (int8_t j = -1; j <= 1; j++)
             {
-                for (int8_t j = -1; j <= 1; j++)
+                if (y + j >= 0 && (int16_t)y + j < yMax)
                 {
                     (*pGrid)[x + i][y + j]++;
                 }
             }
+        }
     }
 }
