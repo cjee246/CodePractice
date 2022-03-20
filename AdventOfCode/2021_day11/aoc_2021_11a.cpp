9a6e23c548dbbe2929cb2675794751da2023aab4
Calvin Jee <calvin.jee@tevosol.com>
2022-03-20 10:52:35 -0600

    aoc_2021_11a - rough stab at first pass of energy inc

--- AdventOfCode/2021_day11/aoc_2021_11a.cpp
+++ AdventOfCode/2021_day11/aoc_2021_11a.cpp
@@ -23,12 +23,14 @@ using namespace std;
 /******************************************************************************/
 /* GLOBAL VARS */
 /******************************************************************************/
-#define VAR 0
+#define STEPS 10U
 
 /******************************************************************************/
 /* FUNCTION DECLARATIONS */
 /******************************************************************************/
-static void func();
+static void doStep(vector<vector<uint8_t>> *pGrid,
+                   vector<vector<uint8_t>> *pExcite,
+                   vector<vector<uint8_t>> *pFlash);
 
 /******************************************************************************/
 /* MAIN */
@@ -43,8 +45,37 @@ int main()
         fileInput.close();
         return 0;
     }
+
+    // initialize grids
     vector<vector<uint8_t>> vecGrid;
     getGrid(fileInput, &vecGrid);
+    vector<vector<uint8_t>> excGrid(vecGrid.size(), vector<uint8_t>(vecGrid[0].size(), 0));
+    vector<vector<uint8_t>> flashGrid = excGrid;
+
+    /*
+    plan
+    - maps
+        1) current grid
+        2) excitation grid
+        3) flash grid
+    - loop
+        - increase a thing
+        - if > 9, update excitation grid for all adjacents
+            - ++ for flash grid
+        - do for all things
+        - count flash grid
+            - if more non-zeroes than before redo loop with excitation
+        - add excitation grid values
+            - reset excitation grid
+    */
+
+    // do stuff
+    bool update = true;
+    uint64_t count = 0;
+    for (uint16_t i = 0; i < STEPS; i++)
+    {
+        doStep(&vecGrid, &excGrid, &flashGrid);
+    }
 
     // print and exit
     cout << '\n';
@@ -55,7 +86,34 @@ int main()
 /******************************************************************************/
 /* FUNCTION DEFINITIONS */
 /******************************************************************************/
-static void func()
+static void doStep(vector<vector<uint8_t>> *pGrid,
+                   vector<vector<uint8_t>> *pExcite,
+                   vector<vector<uint8_t>> *pFlash)
 {
+    
+    for (uint16_t i = 0; i < (*pGrid).size(); i++)
+    {
+        for (uint16_t j = 0; j < (*pGrid)[i].size(); j++)
+        {
+            (*pGrid)[i][j]++;
+            if ((*pGrid)[i][j] >= 9)
+            {
+            }
+        }
+    }
+}
 
+static void incAdj(vector<vector<uint8_t>> *pGrid, uint8_t x, uint8_t y)
+{
+    uint16_t xMax = (*pGrid).size();
+    for (int8_t i = -1; i <= 1; i++)
+    {
+        if ((x + i >= 0) && (x + i < (*pGrid).size()))
+            {
+                for (int8_t j = -1; j <= 1; j++)
+                {
+                    (*pGrid)[x + i][y + j]++;
+                }
+            }
+    }
 }
