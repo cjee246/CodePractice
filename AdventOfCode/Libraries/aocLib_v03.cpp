576a4dd8000b8d2caeda054499dfd7de4a2e8db6
cjee2 <cjee2@DESKTOP-G9F0T51>
2022-03-24 19:42:21 -0600

    Library update with PlotVec() and proper casing

---
+++ AdventOfCode/Libraries/aocLib_v03.cpp
@@ -0,0 +1,121 @@
+
+/*
+ * Author: Calvin Jee
+ * Email: cjee246@gmail.com
+ *
+ * Date: 2022-03-24
+ */
+
+/******************************************************************************/
+/* HEADER FILES */
+/******************************************************************************/
+#include "aocLib_v03.h"
+using namespace std;
+
+/******************************************************************************/
+/* FUNCTIONS */
+/******************************************************************************/
+namespace aocLib_v03
+{
+    /*!
+     *  @brief      Function description
+     *  @param      parameter description
+     */
+    void ExampleFunc(int parameter) {}
+
+    int LoadFile(ifstream &rFilestream)
+    {
+        // read text file
+        string inputName;
+        cout << "Enter filename.\n";
+        cin >> inputName;
+        rFilestream.open(inputName);
+        while (!rFilestream)
+        {
+            cout << "File does not exist. Enter filename or type 'q' to quit.\n";
+            cin >> inputName;
+            if (inputName == "q")
+            {
+                return 1;
+            }
+            rFilestream.open(inputName);
+        }
+        return 0;
+    }
+
+    int LoadFile(ifstream &rFilestream, string &rFilestr)
+    {
+        // read text file
+        string inputName;
+        cout << "Enter filename.\n";
+        cin >> inputName;
+        rFilestream.open(inputName);
+        while (!rFilestream)
+        {
+            cout << "File does not exist. Enter filename or type 'q' to quit.\n";
+            cin >> inputName;
+            if (inputName == "q")
+            {
+                return 1;
+            }
+            rFilestream.open(inputName);
+        }
+        rFilestr = inputName;
+        return 0;
+    }
+
+    void GetStrVec(ifstream &rFilestream, vector<string> &rVecStr)
+    {
+        string substr;
+        while (getline(rFilestream, substr))
+        {
+            if (substr.size() > 0)
+            {
+                rVecStr.push_back(substr);
+            }
+        }
+    }
+
+    void GetGrid(ifstream &rFilestream, vector<vector<uint8_t>> &rVecGrid)
+    {
+        char fileChar;
+        uint64_t row = 0;
+        rVecGrid.push_back(vector<uint8_t>());
+        while (rFilestream.get(fileChar))
+        {
+            if (isdigit(fileChar))
+            {
+                rVecGrid[row].push_back(stoi(&fileChar));
+            }
+            else
+            {
+                row++;
+                rVecGrid.push_back(vector<uint8_t>());
+            }
+        }
+        rVecGrid.pop_back();
+    }
+
+    void PlotVec(string &str, char delim, vector<vector<uint16_t>> &rVecPlot)
+    {
+        stringstream strStream(str);
+        string xStr, yStr;
+        uint16_t x, y;
+        getline(strStream, xStr, delim);
+        getline(strStream, yStr);
+        x = stoi(xStr);
+        y = stoi(yStr);
+        if (rVecPlot.size() < x + 1)
+        {
+            rVecPlot.resize(x + 1);
+        }
+        for (auto &yvec : rVecPlot)
+        {
+            if (yvec.size() < y + 1)
+            {
+                yvec.resize(y + 1);
+            }
+        }
+        rVecPlot[x][y]++;
+    }
+}
