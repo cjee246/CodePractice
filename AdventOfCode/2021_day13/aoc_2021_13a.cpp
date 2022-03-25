e5bca4ae5523f53938a041f50e2f9e08c360cd14
Calvin Jee <calvin.jee@tevosol.com>
2022-03-24 19:09:54 -0600

    aoc_2021_13a - setup

---
+++ AdventOfCode/2021_day13/aoc_2021_13a.cpp
@@ -0,0 +1,60 @@
+/*
+ * Author: Calvin Jee
+ * Email: cjee246@gmail.com
+ *
+ * Date: 2022-03-24
+ */
+
+/******************************************************************************/
+/* INCLUDES & NAMESPACES */
+/******************************************************************************/
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include "../Libraries/aocLib_v02.h"
+
+using namespace std;
+using namespace aocLib_v02;
+
+/******************************************************************************/
+/* GLOBAL VARS */
+/******************************************************************************/
+#define VAR 0
+
+/******************************************************************************/
+/* FUNCTION DECLARATIONS */
+/******************************************************************************/
+static void func();
+
+/******************************************************************************/
+/* MAIN */
+/******************************************************************************/
+int main()
+{
+    // read file
+    std::ios::sync_with_stdio(false);
+    ifstream fileInput;
+    if (loadFile(fileInput))
+    {
+        fileInput.close();
+        return 0;
+    }
+
+    // print and exit
+    cout << '\n';
+    fileInput.close();
+    return 0;
+}
+
+/******************************************************************************/
+/* FUNCTION DEFINITIONS */
+/******************************************************************************/
+static void func()
+{
+
+}
