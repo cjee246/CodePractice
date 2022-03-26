6de61c56dec07e8f5ba682bd085c46eca450713d
Calvin Jee <calvin.jee@tevosol.com>
2022-03-26 17:28:10 -0600

    aoc_2021_14b - setup 2D indexing and count - example confirmed - input incorrect (???)

--- AdventOfCode/2021_day14/aoc_2021_14b.cpp
+++ AdventOfCode/2021_day14/aoc_2021_14b.cpp
@@ -34,10 +34,16 @@ static vector<char> GetUnique(string first, vector<vector<string>> &rVecRules);
 static void Polymerize(string &rPoly, vector<vector<string>> &rVecRules);
 static uint64_t FinalCount(const string &rPoly, vector<char> &rUnique, vector<uint64_t> &rCount);
 
-static void GetVecIdx(vector<vector<string>> &rVecRules, vector<uint8_t> &rVecIdx);
-static void InitVecIdx(const string &rPoly, vector<vector<uint64_t>> &rVecCount,
+static void GetVecIdx(vector<vector<string>> &rVecRules, vector<vector<uint8_t>> &rVecIdx);
+static void InitVecIdx(const string &rPoly, vector<uint64_t> &rVecCount,
                        vector<vector<string>> &rVecRules);
-static void StepVecIdx(vector<vector<uint64_t>> &rVecCount, const vector<uint8_t> &rVecIdx);
+static void StepVecIdx(vector<uint64_t> &rVecCount,
+                       const vector<vector<uint8_t>> &rVecIdx);
+static uint64_t CountIdx(const vector<uint64_t> &rVecCount1,
+                         const vector<string> &rVecRules1,
+                         const vector<char> &rUnique,
+                         vector<uint64_t> &rVecTally,
+                         string &rFirst);
 
 /******************************************************************************/
 /* MAIN */
@@ -57,25 +63,25 @@ int main()
     vector<string> vecString;
     vector<vector<string>> vecRules;
     GetStrVec(fileInput, vecString);
-    string poly = ParseStrVec(vecString, vecRules);
-    vector<char> vecUnique = GetUnique(poly, vecRules);
+    string first = ParseStrVec(vecString, vecRules);
+    vector<char> vecUnique = GetUnique(first, vecRules);
     vector<uint64_t> vecTally(vecUnique.size(), 0);
 
     // do pair count indexing
-    vector<vector<uint64_t>> vecCount(vecRules.size(),
-                                      vector<uint64_t>(vecRules[0].size()));
-    vector<uint8_t> vecIdx(vecRules[0].size());
+    vector<uint64_t> vecCount(vecRules[0].size());
+    vector<vector<uint8_t>> vecIdx(2, vector<uint8_t>(vecRules[0].size()));
     GetVecIdx(vecRules, vecIdx);
 
     // loop through pair counting index
-    InitVecIdx(poly, vecCount, vecRules);
+    InitVecIdx(first, vecCount, vecRules);
     for (uint8_t i = 0; i < STEPS; i++)
     {
         StepVecIdx(vecCount, vecIdx);
+        //CountIdx(vecCount, vecRules[0], vecUnique, vecTally, first);
     }
 
     // print and exit
-    cout << FinalCount(poly, vecUnique, vecTally) << '\n';
+    cout << CountIdx(vecCount, vecRules[0], vecUnique, vecTally, first) << '\n';
     fileInput.close();
     return 0;
 }
@@ -161,20 +167,27 @@ static uint64_t FinalCount(const string &rPoly, vector<char> &rUnique, vector<ui
     return max - min;
 }
 
-static void GetVecIdx(vector<vector<string>> &rVecRules, vector<uint8_t> &rVecIdx)
+static void GetVecIdx(vector<vector<string>> &rVecRules, vector<vector<uint8_t>> &rVecIdx)
 {
     vector<string>::iterator it;
     for (uint8_t i = 0; i < rVecRules[0].size(); i++)
     {
+        // front pair
         string str = "";
         str.push_back(rVecRules[0][i][0]);
         str.push_back(rVecRules[1][i][0]);
         it = find(rVecRules[0].begin(), rVecRules[0].end(), str);
-        rVecIdx[i] = it - rVecRules[0].begin();
+        rVecIdx[0][i] = it - rVecRules[0].begin();
+
+        // back pair
+        str.erase(str.begin());
+        str.push_back(rVecRules[0][i][1]);
+        it = find(rVecRules[0].begin(), rVecRules[0].end(), str);
+        rVecIdx[1][i] = it - rVecRules[0].begin();
     }
 }
 
-static void InitVecIdx(const string &rPoly, vector<vector<uint64_t>> &rVecCount,
+static void InitVecIdx(const string &rPoly, vector<uint64_t> &rVecCount,
                        vector<vector<string>> &rVecRules)
 {
     string substr;
@@ -185,15 +198,58 @@ static void InitVecIdx(const string &rPoly, vector<vector<uint64_t>> &rVecCount,
         substr = rPoly.substr(i, 2);
         it = find(rVecRules[0].begin(), rVecRules[0].end(), substr);
         idx = it - rVecRules[0].begin();
-        rVecCount[0][idx]++;
+        rVecCount[idx]++;
     }
 }
 
-static void StepVecIdx(vector<vector<uint64_t>> &rVecCount, const vector<uint8_t> &rVecIdx)
+static void StepVecIdx(vector<uint64_t> &rVecCount,
+                       const vector<vector<uint8_t>> &rVecIdx)
 {
-    for (uint8_t i = 0; i < rVecCount[0].size(); i++)
+    vector<uint64_t> vecNew(rVecCount.size(), 0);
+    for (uint8_t i = 0; i < rVecCount.size(); i++)
     {
-        rVecCount[1][rVecIdx[i]] += rVecCount[0][i];
+        vecNew[rVecIdx[0][i]] += rVecCount[i];
+        vecNew[rVecIdx[1][i]] += rVecCount[i];
     }
-    rVecCount[0] = rVecCount[1];
+    rVecCount = vecNew;
+}
+
+static uint64_t CountIdx(const vector<uint64_t> &rVecCount1,
+                         const vector<string> &rVecRules1,
+                         const vector<char> &rUnique,
+                         vector<uint64_t> &rVecTally,
+                         string &rFirst)
+{
+    uint64_t current = 0, min = 0, max = 0;
+    for (uint8_t i = 0; i < rUnique.size(); i++)
+    {
+        current = 0;
+        uint8_t idx;
+        if (rUnique[i] == rFirst.front() || rUnique[i] == rFirst.back())
+        {
+            current++;
+        }
+        for (uint8_t j = 0; j < rVecRules1.size(); j++)
+        {
+            if (rVecRules1[j].front() == rUnique[i])
+            {
+                current += rVecCount1[j];
+            }
+            if (rVecRules1[j].back() == rUnique[i])
+            {
+                current += rVecCount1[j];
+            }
+        }
+        current /= 2;
+        rVecTally[i] = current;
+        if (current > max)
+        {
+            max = current;
+        }
+        if (current < min || min == 0)
+        {
+            min = current;
+        }
+    }
+    return max - min;
 }
