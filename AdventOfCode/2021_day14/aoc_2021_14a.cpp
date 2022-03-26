ed4288f83347931faad583a8e9c5d75abd930f2d
Calvin Jee <calvin.jee@tevosol.com>
2022-03-26 14:29:56 -0600

    aoc_2021_14a - setup parse data

--- AdventOfCode/2021_day14/aoc_2021_14a.cpp
+++ AdventOfCode/2021_day14/aoc_2021_14a.cpp
@@ -24,12 +24,13 @@ using namespace aocLib_v03;
 /******************************************************************************/
 /* GLOBAL VARS */
 /******************************************************************************/
-#define VAR 0
+#define STEPS 10
 
 /******************************************************************************/
 /* FUNCTION DECLARATIONS */
 /******************************************************************************/
-static void Func();
+static string ParseStrVec(vector<string> &rVecStr, vector<vector<string>> &rVecRules);
+static void Polymerize(string &rPoly, const vector<vector<string>> &rVecRules);
 
 /******************************************************************************/
 /* MAIN */
@@ -44,6 +45,16 @@ int main()
         fileInput.close();
         return 0;
     }
+    vector<string> vecString;
+    vector<vector<string>> vecRules;
+    GetStrVec(fileInput, vecString);
+    string first = ParseStrVec(vecString, vecRules);
+
+    // do insertion for loop
+    for (uint8_t i = 0; i < STEPS; i++)
+    {
+        Polymerize(first, vecRules);
+    }
 
     // print and exit
     cout << '\n';
@@ -54,7 +65,19 @@ int main()
 /******************************************************************************/
 /* FUNCTION DEFINITIONS */
 /******************************************************************************/
-static void Func()
+static string ParseStrVec(vector<string> &rVecStr, vector<vector<string>> &rVecRules)
 {
+    string first = rVecStr[0];
+    rVecStr.erase(rVecStr.begin());
+    for (uint8_t i = 0; i < rVecStr.size(); i++)
+    {
+        rVecRules.push_back(vector<string>());
+        rVecRules[i].push_back(rVecStr[i].substr(0, 2));
+        rVecRules[i].push_back(rVecStr[i].substr(6, 1));
+    }
+    return first;
+}
 
+static void Polymerize(string &rPoly, const vector<vector<string>> &rVecRules)
+{
 }
