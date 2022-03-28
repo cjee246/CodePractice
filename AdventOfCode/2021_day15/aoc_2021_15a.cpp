8a331d43bd5ff94ed0edfd624288f4933a23c922
Calvin Jee <calvin.jee@tevosol.com>
2022-03-28 17:40:13 -0600

    aoc_2021_15a - setup neighbor checking in main

--- AdventOfCode/2021_day15/aoc_2021_15a.cpp
+++ AdventOfCode/2021_day15/aoc_2021_15a.cpp
@@ -29,7 +29,10 @@ using namespace aocLib_v03;
 /******************************************************************************/
 /* FUNCTION DECLARATIONS */
 /******************************************************************************/
-static void Func();
+static vector<uint16_t> CheckNeighbors(vector<vector<uint8_t>> &rVecGrid,
+                                       vector<vector<uint8_t>> &rVecVisited,
+                                       vector<vector<uint16_t>> &rVecDist,
+                                       vector<uint16_t> nodeCoords);
 
 /******************************************************************************/
 /* MAIN */
@@ -44,6 +47,19 @@ int main()
         fileInput.close();
         return 0;
     }
+    vector<vector<uint8_t>> vecGrid;
+    GetGrid(fileInput, vecGrid);
+
+    // setup tracking vectors
+    vector<vector<uint8_t>> vecVisited(vecGrid.size(), vector<uint8_t>(vecGrid[0].size(), 0));
+    vector<vector<uint16_t>> vecDist(vecGrid.size(), vector<uint16_t>(vecGrid[0].size(), 60000));
+    vector<vector<uint16_t>> vecPath;
+
+    vector<uint16_t> newCoord;
+    for (uint32_t i = 0; i < vecPath.size(); i++)
+    {
+        newCoord = CheckNeighbors(vecGrid, vecVisited, vecDist, vecPath[i]);
+    }
 
     // maybe see here: https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
     // also this for windows printing variables: https://stackoverflow.com/questions/62537957/vs-code-local-variables-not-showing-correctly
@@ -57,7 +73,32 @@ int main()
 /******************************************************************************/
 /* FUNCTION DEFINITIONS */
 /******************************************************************************/
-static void Func()
+static vector<uint16_t> CheckNeighbors(vector<vector<uint8_t>> &rVecGrid,
+                                       vector<vector<uint8_t>> &rVecVisited,
+                                       vector<vector<uint16_t>> &rVecDist,
+                                       vector<uint16_t> nodeCoords)
 {
+    uint16_t x = nodeCoords[0], y = nodeCoords[1];
+    if (x < rVecDist.size() - 1)
+    {
+        UpdateDist(rVecDist, nodeCoords, +1, 0);
+    }
+    if (x > 0)
+    {
+        UpdateDist(rVecDist, nodeCoords, -1, 0);
+    }
+    if (y < rVecDist[0].size() - 1)
+    {
+        UpdateDist(rVecDist, nodeCoords, 0, +1);
+    }
+    if (y > 0)
+    {
+        UpdateDist(rVecDist, nodeCoords, 0, -1);
+    }
+}
 
+static void UpdateDist(vector<vector<uint16_t>> &rVecDist,
+                       vector<uint16_t> nodeCoords,
+                       uint8_t xOffset, uint8_t yOffset)
+{
 }
