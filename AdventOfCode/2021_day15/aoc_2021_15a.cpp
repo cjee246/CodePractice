bc3b9159b5eb10bb3555736e15f01d6608704bae
Calvin Jee <calvin.jee@tevosol.com>
2022-03-28 11:58:13 -0600

    aoc_2021_15a - setup for work later

---
+++ AdventOfCode/2021_day15/aoc_2021_15a.cpp
@@ -0,0 +1,63 @@
+/*
+ * Author: Calvin Jee
+ * Email: cjee246@gmail.com
+ *
+ * Date: 2022-03-30
+ */
+
+/******************************************************************************/
+/* INCLUDES & NAMESPACES */
+/******************************************************************************/
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include "../Libraries/aocLib_v03.h"
+
+using namespace std;
+using namespace aocLib_v03;
+
+/******************************************************************************/
+/* GLOBAL VARS */
+/******************************************************************************/
+#define VAR 0
+
+/******************************************************************************/
+/* FUNCTION DECLARATIONS */
+/******************************************************************************/
+static void Func();
+
+/******************************************************************************/
+/* MAIN */
+/******************************************************************************/
+int main()
+{
+    // read file
+    std::ios::sync_with_stdio(false);
+    ifstream fileInput;
+    if (LoadFile(fileInput))
+    {
+        fileInput.close();
+        return 0;
+    }
+
+    // maybe see here: https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+    // also this for windows printing variables: https://stackoverflow.com/questions/62537957/vs-code-local-variables-not-showing-correctly
+
+    // print and exit
+    cout << '\n';
+    fileInput.close();
+    return 0;
+}
+
+/******************************************************************************/
+/* FUNCTION DEFINITIONS */
+/******************************************************************************/
+static void Func()
+{
+
+}
