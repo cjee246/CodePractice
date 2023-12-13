ff4f9577d5029a83f536d5a9aeeca13d692ccb8b
Calvin Jee (XPS 13) <cjee246@gmail.com>
2023-12-12 23:36:24 -0700

    AOC 2023 day 2a verified

--- AdventOfCode/2023_day2/aoc_2023_02a.py
+++ AdventOfCode/2023_day2/aoc_2023_02a.py
@@ -5,11 +5,11 @@ __email__ = "cjee246@gmail.com"
 __date__ = "2023-12-12"
 
 # imports
-# import os
+import re
 
 # define file
-inputFile = 'C:\\Users\\cjee2\\Documents\\Git\\CodePractice\\AdventOfCode\\2023_day2\\example.txt'
-# inputFile = 'C:\\Users\\cjee2\\Documents\\Git\\CodePractice\\AdventOfCode\\2023_day2\\input.txt'
+# inputFile = 'C:\\Users\\cjee2\\Documents\\Git\\CodePractice\\AdventOfCode\\2023_day2\\example.txt'
+inputFile = 'C:\\Users\\cjee2\\Documents\\Git\\CodePractice\\AdventOfCode\\2023_day2\\input.txt'
 
 # Program to read entire file
 file = open(inputFile)
@@ -32,9 +32,24 @@ Plan:
     - get substring
     - find integer
     - compare with bag key
-    - if exceeds max, record game ID
+    - if not exceeds max, record game ID
 - count all in game ID list
 """
 
+idList = []
 for line in lines:
+    possible = True
+    line = line.strip()
+    line = line.replace('Game ', '')
+    sets = re.split(': |; ', line)
+    for i in range(1, len(sets)):
+        draw = sets[i].split(', ')
+        for color in draw:
+            cubes = color.split(' ')
+            if int(cubes[0]) > bag[cubes[1]]:
+                possible = False
+    if possible:
+        idList.append(int(sets[0]))        
+print(sum(idList))
+
 
